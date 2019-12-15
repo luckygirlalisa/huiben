aba25854ae7e9e9d058330627e3ffb2988f5ef11
zfxiao <luckygirlalisa@gmail.com>
2019-12-15 15:23:18 +0800

    Show join-us as coming.

--- src/pages/club/index.vue
+++ src/pages/club/index.vue
@@ -1,18 +1,38 @@
 <template>
-  <PageContent>
-    <div class="content">
-      <p class="text">敬请期待</p>
-    </div>
-  </PageContent>
+   <PageContent>
+       <LogoHeader />
+       <div class="content">
+         <p class="text">敬请期待</p>
+       </div>
+   </PageContent>
 </template>
 
 <script>
+  import PageContent from '@/components/PageContent'
+  import LogoHeader from '@/components/LogoHeader'
+
+  export default {
+    data: {
+      images: [
+        '/static/images/logo.png'
+      ],
+      name: '',
+      school: '',
+      phone: '',
+      home: ''
+    },
+    components: {
+      PageContent,
+      LogoHeader
+    }
+  }
 </script>
 
 <style>
-  .content {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
+
